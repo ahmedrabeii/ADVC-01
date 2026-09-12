1310f12efd1e7e5677bfe10074efbf59ef216a42
ahmeddrabeii <ahma456ad@gmail.com>
2026-09-12 03:32:40 +0300

    Q1

--- ADV C#01/Program.cs
+++ ADV C#01/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Def:Generics allow you to write classes, interfaces and with type parameters.
+            //The actual is specified when the is used,not when it is written.
+            //Use:Type Safety, Code Reusability, Better Performance, Cleaner Code
+            #endregion
         }
     }
 }
